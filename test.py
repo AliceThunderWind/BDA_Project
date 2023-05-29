4ebf5f8567ff825abf97739313605a85dee551c0
killianvervelle <115397396+killianvervelle@users.noreply.github.com>
2023-05-29 15:46:13 +0200

    h5 to csv to parquet

--- test.py
+++ test.py
@@ -1,5 +1,13 @@
 import hdf5_getters
-h5 = hdf5_getters.open_h5_file_read("C:\\Users\\dalia\\Documents\\DMH\\04 HES-SO Master\\S2\\MA_BDA\\Projet\\BDA_Project\\MillionSongSubset\\B\\B\\B\\TRBBBLA128F424E963.h5")
-duration = hdf5_getters.get_artist_id(h5)
-print(duration)
-h5.close()
+from utils import *
+from hdf5_getters import *
+    
+files = get_all_files('data',ext='.h5')
+
+# Open the .h5 file in read-only mode
+for file in files[:1]:
+    data = hdf5_getters.open_h5_file_read(file)
+    print(data)
+    artist_id = hdf5_getters.get_artist_id(data)
+    artist_lat = hdf5_getters.get_artist_latitude(data)
+    print(artist_lat)
